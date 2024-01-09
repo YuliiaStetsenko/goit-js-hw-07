d762123be52dc684122da4848b019f9b1bbd4e2c
Юлія <109523518+YuliiaStetsenko@users.noreply.github.com>
2024-01-09 14:20:43 +0200

    +++
    +++

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const loginForm = document.querySelector('.login-form');
 
+  loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const emailInput = document.querySelector('[name="email"]');
+    const passwordInput = document.querySelector('[name="password"]');
+    
+    if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
+      alert('All form fields must be filled in');
+      return;
+    }
+
+    const formData = {
+      email: emailInput.value.trim(),
+      password: passwordInput.value.trim()
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+  });
+});
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const bodyElement = document.body;
+  const colorSpan = document.querySelector('.color');
+  const changeColorButton = document.querySelector('.change-color');
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
+
+  changeColorButton.addEventListener('click', function () {
+    const randomColor = getRandomHexColor();
+    bodyElement.style.backgroundColor = randomColor;
+    colorSpan.textContent = randomColor;
+  });
+});
